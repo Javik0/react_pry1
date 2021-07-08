e5d26a6adf4b26a508da3f967e63c13c50226a38
Javik0 <javico.davila@gmail.com>
2021-07-07 21:09:14 -0500

    react 2

--- proy1/src/App.js
+++ proy1/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import primer from './imgs/react1.jpg';
+import segundo from './imgs/react2.png';
+import tercero from './imgs/react3.jpg';
+//import practic1 from '.datos/prectic1.json';
+
+class CambiarImagenUno extends React.Component {
+
+  state={
+    bandera : true
+  }
+  imagenUno = ()=>{
+    this.setState({bandera:!this.state.bandera});
+  }
+
+  imagenDos = ()=>{
+    this.setState({bandera:!this.state.bandera});
+  }
+  render(){
+    if(this.state.bandera){
+      return (<div>
+        <button onClick={this.imagenUno} >Imagen Uno</button></div>
+      )
+    }else{
+      return <div><h1><img src={primer}></img></h1><button onClick={this.imagenUno} >Imagen Uno</button></div>
+    }
+  }
+}
+
+
+class CambiarImagenDos extends React.Component {
+
+  state={
+    bandera : true
+  }
+  imagenDos = ()=>{
+    this.setState({bandera:!this.state.bandera});
+  }
+  render(){
+    if(this.state.bandera){
+      return (<div>
+        <button onClick={this.imagenDos} >Imagen Dos</button></div>
+      )
+    }else{
+      return <div><h1><img src={segundo}></img></h1><button onClick={this.imagenDos} >Imagen Dos</button></div>
+    }
+  }
+}
+
+class CambiarImagenTres extends React.Component {
+
+  state={
+    bandera : true
+  }
+  imagenTres = ()=>{
+    this.setState({bandera:!this.state.bandera});
+  }
+  render(){
+    if(this.state.bandera){
+      return (<div>
+        <button onClick={this.imagenTres} >Imagen Tres</button></div>
+      )
+    }else{
+      return <div><h1><img src={tercero}></img></h1><button onClick={this.imagenTres} >Imagen Tres</button></div>
+    }
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div id="principal">
+    <h1 id="imagenUno" > 
+      <CambiarImagenUno/>
+      <CambiarImagenDos/>
+      <CambiarImagenTres/>
+      
+    </h1>
+   
+  </div>);
 }
+
 
 export default App;
